98a2c0b98bd7db938a60306a9bca0a972e39a1a7
Neonscape <neonscapemail@gmail.com>
2025-01-16 22:41:58 +0800

    fixed problem in initialization

--- src/db.py
+++ src/db.py
@@ -12,7 +12,7 @@ class Database:
 
     def __init__(self, db_path):
         try:
-            self.conn = sqlite3.connect(db_path)
+            self.conn = sqlite3.connect(db_path, check_same_thread=False)
             self.cursor = self.conn.cursor()
             logger.info(f"Connected to database at {db_path}")
         except sqlite3.Error as e:
